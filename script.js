9be1bce82726399250ebb8b7c54c105f24bff4c4
elangn <elang.nugraha93@gmail.com>
2024-03-30 21:03:29 +0700

    tes

---
+++ script.js
@@ -0,0 +1,3 @@
+console.log("ini script eksternal");
+// alert("selamat datang ");
+document.write("hello wordl");
---
+++ script2.js
@@ -0,0 +1,14 @@
+function Mahasiswa(nama, jurusan) {
+  this.nama = nama;
+  this.jurusan = jurusan;
+}
+
+// class Mahasiswa {
+//   constructor(nama, jurusan) {
+//     this.nama = nama;
+//     this.jurusan = jurusan;
+//   }
+// }
+
+let mhs1 = new Mahasiswa("elang", "sisfo");
+console.log(mhs1);
